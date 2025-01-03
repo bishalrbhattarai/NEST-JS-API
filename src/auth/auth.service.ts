ec74b5fef59f255926a6d4ec979aaa8efbd5590e
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2025-01-03 14:11:29 +0545

    only sending token in signin module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,7 +20,6 @@ export class AuthService {
         const createdUser = await this.primsa.user.create({
             data: { ...signUpDTO, password: hashedPassword }
         });
-        delete createdUser.password;
         return createdUser
     }
 
