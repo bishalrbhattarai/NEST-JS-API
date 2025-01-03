f1c61c0d59d3256051b72801a8cfeb8aec6a3c20
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2025-01-03 14:09:06 +0545

    imported passport and passport-jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,27 +3,25 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { SignUpDto } from './dto/signup.dto';
 import { SigninDTO } from './dto/signin.dto';
 import * as argon2 from 'argon2';
-import { Prisma } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly primsa: PrismaService) { }
+    constructor(private readonly primsa: PrismaService, private readonly jwt: JwtService) { }
+
+    async signToken(id: number, email: string) {
+        const payload = { email, sub: id }
+        return this.jwt.sign(payload);
+    }
 
     async signUp(signUpDTO: SignUpDto) {
         const hashedPassword = await argon2.hash(signUpDTO.password);
-
         const createdUser = await this.primsa.user.create({
-            data: {
-                ...signUpDTO,
-                password: hashedPassword
-            }
+            data: { ...signUpDTO, password: hashedPassword }
         });
         delete createdUser.password;
-
         return createdUser
-
-
     }
 
     async signIn(signInDTO: SigninDTO) {
@@ -33,20 +31,10 @@ export class AuthService {
                 email
             }
         })
-
-        if (!foundUser) {
-            throw new UnauthorizedException('Invalid credentials')
-        }
-
+        if (!foundUser) throw new UnauthorizedException('Invalid Credentials');
         const isPasswordValid = await argon2.verify(foundUser.password, password);
-
-        if (!isPasswordValid) {
-            throw new UnauthorizedException('Invalid credentials')
-        }
-
-
+        if (!isPasswordValid) throw new UnauthorizedException('Invalid credentials');
         delete foundUser.password;
         return foundUser;
-
     }
 }
